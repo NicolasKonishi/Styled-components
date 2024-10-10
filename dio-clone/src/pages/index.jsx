c94092289136c9af3caa2225f7bc447d145ae42e
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-10-10 12:34:30 -0300

    useEffect

--- dio-clone/src/pages/index.jsx
+++ dio-clone/src/pages/index.jsx
@@ -1,21 +1,22 @@
-import {useState} from 'react'
+import { useState, useEffect } from "react";
 
 const Teste = () => {
-    const [name, setName] = useState('Nicolas');
-  
-    const handleChangeName= () => {
-        setName(prev => prev === 'Nicolas' ? 'Jao':'Nicolas');
-    }
-    return (
-    <div>
-        <p>
-        {name}
-        </p>
-        <button onClick={handleChangeName}> 
-            Alterar
-        </button>
-    </div>
-  )
-}
+  const [name, setName] = useState("Nicolas");
 
-export{Teste};
+  const handleChangeName = () => {
+    setName((prev) => (prev === "Nicolas" ? "Jao" : "Nicolas"));
+  };
+
+  useEffect(() => {
+    alert("renderizei");
+  }, []);
+
+  return (
+    <div>
+      <p>{name}</p>
+      <button onClick={handleChangeName}>Alterar</button>
+    </div>
+  );
+};
+
+export { Teste };
